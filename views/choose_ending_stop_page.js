766c4247349f031ac0a17fd26a7c1c1d34261e9c
meagles <mitch.d.eagles@gmail.com>
2017-06-04 11:55:46 -0500

    fixing state for choosing ending stop

--- views/choose_ending_stop_page.js
+++ views/choose_ending_stop_page.js
@@ -11,21 +11,43 @@ import {
 import _ from 'lodash';
 
 import ProgressBar from './progress_bar';
-import StopsOnThisRoute from './stops_on_this_route'
+import StopsOnThisRoute from './stops_on_this_route';
+import CallApi from '../js/api';
 
 const ChooseEndingStopPage = React.createClass({	
 	
+	getInitialState: function(){
+		return {
+			"AllStops":[]
+		}
+	},
+
+	componentWillMount: function(){
+
+		var SetAllStops = _.bind(this.setState, this)
+
+		CallApi("GetAllStopsForTrip", {TripId:this.props.TripId}, function(Result){
+			SetAllStops({AllStops:Result});
+		});
+
+	},
+
 	render: function(){
 
-		return(
-			<View style={{flexDirection:'column', justifyContent:'flex-end'}}>
-				<ProgressBar style={{height:100}} CurrentStep={3} />
-				<View style={styles.GreenBox}>
-					<Text style={{color:'white', fontSize:24, fontWeight:'bold'}}>Where are you getting off?</Text>
+		if(this.state.AllStops == []){
+			return (<View></View>);
+		} else {
+
+			return(
+				<View style={{flexDirection:'column', justifyContent:'flex-end'}}>
+					<ProgressBar style={{height:100}} CurrentStep={3} />
+					<View style={styles.GreenBox}>
+						<Text style={{color:'white', fontSize:24, fontWeight:'bold'}}>Where are you getting off?</Text>
+					</View>
+					<StopsOnThisRoute UpcomingStops={null /*UpcomingStops*/}/>
 				</View>
-				<StopsOnThisRoute />
-			</View>
-		);
+			);
+		}
 	}
 
 });
--- views/choose_route_page.js
+++ views/choose_route_page.js
@@ -19,7 +19,7 @@ const ChooseRoutePage = React.createClass({
 	
 	getInitialState: function(){
 		return {
-			"NearbyRoutes":null
+			"NearbyRoutes":[]
 		}
 	},
 
